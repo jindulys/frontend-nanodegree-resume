2b5b4274ba1dd6324fe1296eb606f601e9a61429
yansong li <857367901@qq.com>
2015-10-07 23:54:19 -0400

    Lesson1 append Name and Role

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,8 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ var formattedName = HTMLheaderName.replace("%data%", "Yansong Li");
+ var formattedRole = HTMLheaderRole.replace("%data%", "iOS Engineer");
+
+ $("#header").append(formattedName).append(formattedRole);
+ 
